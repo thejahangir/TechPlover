faa02faeff2365b81b3c55a7c98a739a2e1a982d
thejahangir <jahangirxl@gmail.com>
2026-01-23 00:19:10 +0530

    deploye

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  
     const env = loadEnv(mode, '.', '');
     return {
       server: {
+        base: "/TechPlover/",
         port: 3000,
         host: '0.0.0.0',
       },
